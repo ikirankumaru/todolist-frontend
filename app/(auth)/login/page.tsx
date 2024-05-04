64db6ee3e4454ff833e726a2d1e6344a298a640d
KIRAN KUMAR <kiran.k@armiasystems.com>
2024-05-04 17:42:40 +0530

    Setup a basic login form

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import AcmeLogo from '@/app/ui/tdl-logo';
+import LoginForm from '@/app/ui/login-form';
+ 
+export default function LoginPage() {
+  return (
+    <main>      
+        <LoginForm />
+    </main>
+  );
+}
---
+++ app/ui/tdl-logo.tsx
@@ -0,0 +1,9 @@
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`flex flex-row items-center leading-none text-white`}
+    >      
+      <p className="text-[44px]">Todo List App</p>
+    </div>
+  );
+}
